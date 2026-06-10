464bb325ddd06d79c409f0d17655a9be75c9d53e
moonmoonCL <1441545405@qq.com>
2026-06-10 22:23:53 +0800

    add ollama pi alias
    local model pi agent alias

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -23,6 +23,8 @@ abbr -a y yy
 
 #pi-agent
 abbr pp "pi --no-session"
+abbr piq "pi --model SetneufPT/Qwen3.6-27B.MTP_Q3_32K_16GB-GPU"
+abbr ppq "pi --no-session --model SetneufPT/Qwen3.6-27B.MTP_Q3_32K_16GB-GPU"
 
 #just-talk
 abbr just-talk-s "nohup just-talk --no-tui >/dev/null 2>&1 &"
