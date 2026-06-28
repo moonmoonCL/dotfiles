6479f7502eb484519ba0b19f88a824ac3c854194
moonmoon <moonmoonCL@users.noreply.github.com>
2026-06-28 11:04:25 +0800

    add chrome dap alias

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -28,3 +28,8 @@ abbr ppq "pi --no-session --model SetneufPT/Qwen3.6-27B.MTP_Q3_32K_16GB-GPU"
 
 #just-talk
 abbr just-talk-s "nohup just-talk --no-tui >/dev/null 2>&1 &"
+
+#chrome DAP
+abbr chromedap "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+     --remote-debugging-port=9222 \
+     --user-data-dir=/tmp/agent-chrome-profile"
